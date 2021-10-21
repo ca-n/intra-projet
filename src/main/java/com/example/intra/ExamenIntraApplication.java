b6a458ff00056fb5394197d6d0b3a5fb1866cc82
Neil Carrie <neil.carrie@gmail.com>
2021-10-21 15:02:43 -0400

    Add base project & Guess model

---
+++ src/main/java/com/example/intra/ExamenIntraApplication.java
@@ -0,0 +1,13 @@
+package com.example.intra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenIntraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenIntraApplication.class, args);
+    }
+
+}
